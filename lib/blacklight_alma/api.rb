49fa2c0c13a05e806000138a5e353e8c2b52520f
Jeff Chiu <jeffchiu@upenn.edu>
2017-01-12 16:00:47 -0500

    fix for improper handling of response XML when there's only a single subfield inside a datafield

--- lib/blacklight_alma/api.rb
+++ lib/blacklight_alma/api.rb
@@ -64,7 +64,14 @@ module BlacklightAlma
         record['holdings'] = inventory_fields.map do |inventory_field|
           inventory_type = inventory_field['tag']
           subfield_codes_to_fieldnames = Api.inventory_type_to_subfield_codes_to_fieldnames[inventory_type]
-          holding = inventory_field.fetch('subfield', []).reduce(Hash.new) do |acc, subfield|
+
+          # make sure subfields is always an Array (which isn't the case if there's only one subfield element)
+          subfields = inventory_field.fetch('subfield', [])
+          if subfields.is_a?(Hash)
+            subfields = [ subfields ]
+          end
+
+          holding = subfields.reduce(Hash.new) do |acc, subfield|
             fieldname = subfield_codes_to_fieldnames[subfield['code']]
             acc[fieldname] = subfield['__content__']
             acc
