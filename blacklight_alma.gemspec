4371154d7a832c6ad581beefa8cc69ec8897a41b
Jen <tuh42082@temple.edu>
2020-09-16 09:41:12 -0400

    BL-749 Improve logging (#12)
    * BL-749 Improve logging
    
    - Use info logger level rather than debug
    - Updates dependencies to match versions we are using in tul_cob
    
    * Combines multiple logs into one
    
    * Remove apikey from log output

--- blacklight_alma.gemspec
+++ blacklight_alma.gemspec
@@ -17,8 +17,8 @@ Gem::Specification.new do |s|
   s.files = Dir["{app,config,db,lib}/**/*", "LICENSE", "Rakefile", "README.rdoc"]
   s.test_files = Dir["test/**/*"]
 
-  s.add_dependency "rails", ">= 4.2", "< 6"
-  s.add_dependency "blacklight", "~> 6.0"
+  s.add_dependency "rails", ">= 5.2", "< 6"
+  s.add_dependency "blacklight", "~> 7.0"
   # there is no way to point to a git repo in a gemspec,
   # so the app's Gemfile will need to include ezwadl as well.
   s.add_dependency 'ezwadl', '0.0.1'
--- lib/blacklight_alma/base_api.rb
+++ lib/blacklight_alma/base_api.rb
@@ -1,8 +1,8 @@
 
-require 'singleton'
-require 'benchmark'
+require "singleton"
+require "benchmark"
 
-require 'ezwadl'
+require "ezwadl"
 
 module BlacklightAlma
 
@@ -16,11 +16,11 @@ module BlacklightAlma
     # Subclasses should implement this class-level method.
     # @return [String] filename of the wadl to use
     def self.wadl
-      raise 'This should never get called'
+      raise "This should never get called"
     end
 
     def initialize
-      @ezwadl_api = EzWadl::Parser.parse(BlacklightAlma::Engine.root.join('wadl', self.class.wadl))
+      @ezwadl_api = EzWadl::Parser.parse(BlacklightAlma::Engine.root.join("wadl", self.class.wadl))
     end
 
     # Subclasses should implement this class-level method.
@@ -30,19 +30,25 @@ module BlacklightAlma
     # @return [HTTParty::Response] response object
     def request(resource, request_type, params)
       api_params = process_params(params)
-      Blacklight.logger.debug("ALMA API request: wadl=#{self.class.wadl} resource=#{resource.path} params=#{api_params}")
       response = nil
       time = Benchmark.measure do
         response = resource.send(request_type.to_sym, { query: api_params })
       end
-      Blacklight.logger.debug("ALMA API request took (#{(time.real * 1000).to_i}ms)")
+
+      log = {
+        type: "wadl=#{self.class.wadl}",
+        resource: resource.path,
+        params: api_params.except("apikey"),
+        duration: "#{(time.real * 1000).to_i}ms)"
+      }
+      Blacklight.logger.info(log)
       response
     end
 
     def process_params(p)
       params = p.dup
-      if !params.member?('apikey')
-        params['apikey'] = ENV['ALMA_API_KEY']
+      if !params.member?("apikey")
+        params["apikey"] = ENV["ALMA_API_KEY"]
       end
       params
     end
